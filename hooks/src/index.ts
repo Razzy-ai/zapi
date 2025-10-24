3ccccb01dfcac1f397537875babef7c4e7f9fcc7
Rasika <rasikamohite731@gmail.com>
2025-10-24 09:40:04 +0530

    Add webhook endpoint to handle zap run creation

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,15 +1,46 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "../src/generated/prisma";
 
-const app = express()
+const client = new PrismaClient();
+const app = express();
 
-app.post("/hooks/catch/:userId/:zapId" , (req,res) =>{
+app.use(express.json());
 
-     const userId = req.params.userId;
-     const zapId = req.params.zapId;
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId;
+  const zapId = req.params.zapId;
+  const body = req.body;
 
-     //store a new trigger in db
-     //push it on to a queue (kafka/redis)
-     
+  console.log("reached here");
+
+  try {
+    await client.$transaction(async (tx) => {
+      console.log("reached 2");
+
+      const run = await tx.zapRun.create({
+        data: {
+          zapId: zapId,
+          metadata: body,
+        },
+      });
+
+      console.log("reached 3");
+
+      await tx.zapRunOutbox.create({
+        data: {
+          zapRunId: run.id,
+        },
+      });
+    });
+
+    res.json({ message: "Webhook received" });
+  } catch (err) {
+    console.error("Error in transaction:", err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 
-})
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
