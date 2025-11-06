a60a648558b8774f81df64f8c7b668f1903aa64a
Rasika <rasikamohite731@gmail.com>
2025-11-06 17:53:17 +0530

    Setup working pipeline between hooks, DB and kafka processor

---
+++ processor/src/index.ts
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+dotenv.config(); 
+
+import { PrismaClient } from "./generated/prisma/client";
+import { Kafka } from "kafkajs";
+
+const TOPIC_NAME = "zap-events";
+const client = new PrismaClient();
+
+const kafka = new Kafka({
+  clientId: "outbox-processor",
+  brokers: ["localhost:9092"],
+});
+
+async function main() {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  console.log("✅ Processor started...");
+  console.log("Connected to Kafka and DB at:", process.env.DATABASE_URL);
+
+  while (true) {
+    const pendingRows = await client.zapRunOutbox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    if (pendingRows.length === 0) {
+      await new Promise((r) => setTimeout(r, 2000)); // prevent busy loop
+      continue;
+    }
+
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingRows.map((r) => ({
+        value: r.zapRunId,
+      })),
+    });
+
+    await client.zapRunOutbox.deleteMany({
+      where: {
+        id: {
+          in: pendingRows.map((r) => r.id),
+        },
+      },
+    });
+  }
+}
+
+main().catch((err) => {
+  console.error("❌ Error:", err);
+  process.exit(1);
+});
