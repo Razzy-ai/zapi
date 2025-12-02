66e4aa649af030bfaef384bb208438531952b254
Rasika <rasikamohite731@gmail.com>
2025-12-02 21:16:16 +0530

    Added endpoint logic

---
+++ primary-backend/src/router/user.ts
@@ -0,0 +1,99 @@
+import {Router} from "express";
+import { authMiddleware } from "../middleware";
+import { Request,Response } from "express";
+import { SignupSchema , SigninSchema } from "../types";
+import { prismaClient } from "../db";
+import jwt  from "jsonwebtoken";
+import { JWT_PASSWORD } from "../config";
+
+const router = Router();
+
+router.post("/signup" , async(req,res) => {
+    const body = req.body.username
+    const parsedData = SignupSchema.safeParse(body);
+
+    if(!parsedData.success){
+        return res.status(411).json({
+            message:"Incorrect Inputs"
+        })
+    }
+
+    const userExists = await prismaClient.user.findFirst({
+        where: {
+            email: parsedData.data.username
+        }
+    })
+
+    if(userExists){
+        return res.status(403).json({
+            message:"User already exists"
+        })
+    }
+
+    await prismaClient.user.create({
+        data: {
+            email: parsedData.data.username,
+            password: parsedData.data.password,
+            name: parsedData.data.name
+        }
+    })
+ 
+     //await sendEmail
+
+    return res.json({
+        message: "Please verify your account by checking your email"
+    })
+})
+
+router.post("/signin" , async(req,res) => {
+
+    const body = req.body.username
+    const parsedData = SigninSchema.safeParse(body);
+
+    if(!parsedData.success){
+        return res.status(411).json({
+            message:"Incorrect Inputs"
+        })
+    }
+    
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email: parsedData.data.username,
+            password: parsedData.data.password
+        }
+    })
+
+    if(!user){
+        return res.status(403).json({
+            message: "Sorry the credentials are incorrect"
+        })
+    }
+
+    const token = jwt.sign({
+       id: user.id
+    }, JWT_PASSWORD);
+
+    res.json({
+        token: token,
+    })
+})
+
+router.get("/user" ,authMiddleware, async(req,res) => {
+    //@ts-ignore
+    const id = req.id;
+    const user = await prismaClient.user.findFirst({
+        where:{
+            id
+        },
+        select:{
+            name:true,
+            email:true
+        }
+    });
+
+    return res.json({
+        user
+    })
+})
+
+export const userRouter = router
