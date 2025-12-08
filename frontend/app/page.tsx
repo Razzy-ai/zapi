ef3c700331842b853ddb9d2e4702eef753d3cab7
Rasika <rasikamohite731@gmail.com>
2025-12-08 20:16:43 +0530

    Added appbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import { Appbar } from "@/component/Appbar";
 
 export default function Home() {
   return (
     
       <main className="">
-        
+        <Appbar />
+        {/* <Hero />
+        <Hero video/> */}
       </main>
    
   );
